67a9a24e49b6db0f0450fb2cc195edce1cb3d68a
Brandon Diaz <brandon.diaz.abreu@gmail.com>
2018-01-28 20:48:54 -0500

    testing a different transition

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,29 @@ $(document).ready(function() {
   // BarbaJS calls
   if(document.querySelector("#barba-wrapper") && document.querySelector(".barba-container")){
     Barba.Pjax.start();
+    var FadeTransition = Barba.BaseTransition.extend({
+      start: function() {
+        Promise.all([this.newContainerLoading, this.fadeOut()]).then(this.fadeIn.bind(this));
+      },
+      fadeOut: function() {
+        return $(this.oldContainer).animate({ opacity: 0 }).promise();
+      },
+      fadeIn: function() {
+        var _this = this;
+        var $el = $(this.newContainer);
+        $(this.oldContainer).hide();
+        $el.css({
+          visibility : 'visible',
+          opacity : 0
+        });
+        $el.animate({ opacity: 1 }, 400, function() {
+          _this.done();
+        });
+      }
+    });
+    Barba.Pjax.getTransition = function() {
+      return FadeTransition;
+    };
   }
   else{
     console.log("BarbaJS wrappers not found.");
