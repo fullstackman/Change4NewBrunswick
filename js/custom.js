3e82f007a165c9f962dbc6844d1fa81b03c4534c
Brandon Diaz-Abreu <mail@fullstackman.net>
2018-02-26 23:37:50 -0500

    Testing different BarbaJS configuration

--- js/custom.js
+++ js/custom.js
@@ -148,4 +148,33 @@ $(document).ready(function() {
     return false;
   });*/
 
+  let stripeForm = document.getElementById("contributionForm")
+  if(stripeForm){
+    stripeForm.onsubmit=function(e) {
+      e.preventDefault();
+      console.log("\tProcessing a payment...");
+      // Set your secret key: remember to change this to your live secret key in production
+      // See your keys here: https://dashboard.stripe.com/account/apikeys
+      var stripe = require("stripe")("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
+
+      // Token is created using Checkout or Elements!
+      // Get the payment token ID submitted by the form:
+      var token = request.body.stripeToken; // Using Express
+
+      // Charge the user's card:
+      stripe.charges.create({
+        amount: 999,
+        currency: "usd",
+        description: "COntribution to Change for New Brunswick",
+        source: token,
+      }, function(err, charge) {
+        // asynchronously called
+      });
+    }
+  }
+  else{
+    console.log("\tNo form found on this page.");
+  }
+  
+  function getAmount(){return 400;}
 });
---
+++ js/sanityCheck.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+	let dynamicContent = document.querySelector("main div[id^='anymod-']");
+  if(dynamicContent == null && dynamicContent == undefined){
+    console.log("\tFailed to find target content.");
+    return;
+  }
+	if(dynamicContent.innerText.length < 1){
+		dynamicContent.innerText = "We're having trouble loading this content. Please refresh the page.";
+		dynamicContent.classList += " text-center";
+    console.log("\tUpdated the output.");
+	}
+});
+
+function loadAnymod(){
+  var search = window.location.search || ''
+  ,   project = "847N3P"
+  ,   messageObj = { showToolkit: false, links: [] }
+/*
+  if (search.indexOf('project=') > -1) {
+    project = search.split('project=')[1].split('&')[0]
+  }
+
+  if (!project) return console.warn('Missing project')
+  if (!window.parent) return console.warn('No parent window detected')
+  if (!document.referrer) return console.warn('Unable to detect parent window URL')
+*/
+  var decoded = decodeURIComponent(document.cookie).replace(/ /g, '').split(';')
+  ,   referrerParser = document.createElement('a')
+  referrerParser.href = document.referrer
+  var referrerHostname = referrerParser.hostname
+
+  if (!decoded || decoded.length < 1) return
+
+  function processToken (token) {
+    if (!token) return
+    if (token.indexOf('cio.toolkit') === 0 && token.indexOf('|' + project + '|') > -1) {
+      messageObj.activeToken = 'token'
+      messageObj.showToolkit = true
+      return
+    }
+    if (token.indexOf('cio.active.' + project + '|' + referrerHostname) === 0) {
+      messageObj.activeLink = token.split('=')[1]
+      messageObj.showToolkit = true
+      return
+    }
+    if (token.indexOf('cio.solo.' + project + '|' + referrerHostname) === 0) {
+      messageObj.soloInstance = token.split('=')[1]
+      messageObj.showToolkit = true
+      return
+    }
+    if (token.indexOf('cio.link.') !== 0) return
+    var tokenKey = token.split('=')[0]
+    ,   tokenValue = token.split('=')[1]
+    ,   linkIdentifier = tokenKey.split('.')[2]
+    ,   projectIdentifier = tokenKey.split('.')[3]
+    ,   parser = document.createElement('a')
+    parser.href = tokenValue
+    if (projectIdentifier !== project) return
+    if (parser.hostname === referrerHostname) {
+      messageObj.links.push(linkIdentifier)
+    }
+  }
+
+  for (var i = 0; i < decoded.length; i++) {
+    processToken(decoded[i])
+  }
+
+  if (!messageObj.showToolkit) return
+
+  window.parent.window.postMessage(messageObj, '*')
+}
