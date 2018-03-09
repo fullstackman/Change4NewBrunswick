b18b9d6631b1c671d8dd43d1773c91b54ed0cb9e
Brandon Diaz-Abreu <mail@fullstackman.net>
2018-03-08 22:54:56 -0500

    Minor fixes #2

--- js/custom.js
+++ js/custom.js
@@ -90,27 +90,7 @@ $(document).ready(function() {
 
   let stripeForm = document.getElementById("contributionForm")
   if(stripeForm){
-    stripeForm.onsubmit=function(e) {
-      e.preventDefault();
-      console.log("\tProcessing a payment...");
-      // Set your secret key: remember to change this to your live secret key in production
-      // See your keys here: https://dashboard.stripe.com/account/apikeys
-      var stripe = require("stripe")("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
-
-      // Token is created using Checkout or Elements!
-      // Get the payment token ID submitted by the form:
-      var token = request.body.stripeToken; // Using Express
-
-      // Charge the user's card:
-      stripe.charges.create({
-        amount: 999,
-        currency: "usd",
-        description: "COntribution to Change for New Brunswick",
-        source: token,
-      }, function(err, charge) {
-        // asynchronously called
-      });
-    }
+    console.log("\tFound a form.");
   }
   else{
     console.log("\tNo form found on this page.");
